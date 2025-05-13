47d9088c3d567c5334d751746951dfa5bcdbfee4
sbx_user1051 <sbx_user1051@169.254.90.181>
2025-05-13 13:07:37 +0000

    Tue May 13 13:07:37 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,11 +1,12 @@
 
 class SumSolution:
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
     
     def compute(self, x, y):
         return x + y
 
     def sum(self, x, y):
-        def __init__(self, x y):
-            self.x = x
-            self.y = y
-            return compute(self.x, self.y)
+        return x + y
+

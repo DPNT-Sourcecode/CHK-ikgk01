ae5663515c13a35ca1ea0d38e3f9ab317775381a
sbx_user1051 <sbx_user1051@169.254.90.181>
2025-05-13 13:16:37 +0000

    Tue May 13 13:16:37 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,8 @@
 
 class SumSolution:
-    def __init__(self, x:int, y:int):
-        self.x = x
-        self.y = y
     
     def compute(self, x, y):
         return x + y
 
     def sum(self, x, y):
         return x + y
-

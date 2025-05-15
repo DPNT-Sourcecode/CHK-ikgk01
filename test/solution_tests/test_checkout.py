e4d56a86cc3c509b25569981c290a9cf60342ab3
sbx_user1051 <sbx_user1051@169.254.51.215>
2025-05-15 15:12:07 +0000

    Thu May 15 15:12:07 UTC 2025

---
+++ test/solution_tests/test_checkout.py
@@ -0,0 +1,10 @@
+import unittest
+from lib.solutions.CHK.checkout_solution import CheckoutSolution
+
+class TestCheckout(unittest.TestCase):
+    # Happy path testing
+    def test_add(self):
+        self.assertEqual(CheckoutSolution().checkout("r"), -1, "Not correct!")
+        #self.assertEqual(Calculator.add(-6, 4), -2, "Not correct!")
+        #self.assertEqual(Calculator.add(6, -4), 2, "Not correct!")
+        #self.assertEqual(Calculator.add(-6, -4), -10, "Not correct!")

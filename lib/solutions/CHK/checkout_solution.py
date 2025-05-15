c420ab8ea5774887cc5fb85ce194a647f4e02c0b
sbx_user1051 <sbx_user1051@169.254.31.13>
2025-05-15 16:09:45 +0000

    Thu May 15 16:09:45 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,15 @@ class CheckoutSolution:
             "A": 50,
             "B": 30,
             "C": 20,
-            "D": 15 
+            "D": 15,
+            "E": 40
         }
         multibuys = {
-            "A": (3, 130),
-            "B": (2, 45)
+            "A": {3: 130, 5: 200},
+            "B": {2: 45}
+        }
+        bogoff = {
+            "E": {2: {"B": 2}} 
         }
         allowed_characters = list(prices.keys()) #+ [",", " "]
         if len(set(skus) - set(allowed_characters)) != 0:
@@ -20,12 +24,17 @@ class CheckoutSolution:
         for sku in set(skus):
             count = skus.count(sku)
             if count != 0:
-                if sku in multibuys.keys():
+                if sku in multibuys:
                     #if count >= multibuys[sku][0]:
-                    multiple = count // multibuys[sku][0]
+                    for offer in sorted(multibuys[sku], reverse=True):
+                        multiple = count // offer
+                        count = count - multiple * offer[0]
                     total += multiple * multibuys[sku][1]
                     remainder = count % multibuys[sku][0]
                     total += remainder * prices[sku]
                 else:
                     total += count * prices[sku]
         return total
+
+
+print(CheckoutSolution().checkout("AAAABBB"))

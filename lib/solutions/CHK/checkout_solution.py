3c9c9d6428b094dd1b70abee2b2cb0a731c68494
sbx_user1051 <sbx_user1051@169.254.51.215>
2025-05-15 15:15:07 +0000

    Thu May 15 15:15:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,31 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        prices = {
+            "a": 50,
+            "b": 30,
+            "c": 20,
+            "d": 15 
+        }
+        multibuys = {
+            "a": (3, 130),
+            "b": (2, 45)
+        }
+        allowed_characters = list(prices.keys()) + [",", " "]
+        if len(set(skus.lower()) - set(allowed_characters)) != 0:
+            return -1
+        total = 0
+        for sku in set(skus.lower()):
+            count = skus.count(sku)
+            if count != 0:
+                if sku in multibuys.keys():
+                    if count > multibuys[sku][0]:
+                        multiple = count // multibuys[sku][0]
+                        total += multiple * multibuys[sku][1]
+                        remainder = count % multibuys[sku][0]
+                        total += remainder * prices[sku]
+                    else:
+                        total += count * prices[sku]
+        return total
+
+print(CheckoutSolution().checkout("aaa"))

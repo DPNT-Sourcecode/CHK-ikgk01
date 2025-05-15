a5c97f81d8ca55f6279fe515bef4732fc3758130
sbx_user1051 <sbx_user1051@169.254.51.215>
2025-05-15 15:27:07 +0000

    Thu May 15 15:27:07 UTC 2025

--- test/solution_tests/test_checkout.py
+++ test/solution_tests/test_checkout.py
@@ -5,7 +5,12 @@ class TestCheckout(unittest.TestCase):
     # Happy path testing
     def test_add(self):
         self.assertEqual(CheckoutSolution().checkout("r"), -1, "Not correct!")
-        self.assertEqual(CheckoutSolution().checkout("r"), -1, "Not correct!")
+        self.assertEqual(CheckoutSolution().checkout("aaabb"), 175, "Not correct!")
+        self.assertEqual(CheckoutSolution().checkout("aaaabbb"), 255, "Not correct!")
+        self.assertEqual(CheckoutSolution().checkout("abcd"), 115, "Not correct!")
+        self.assertEqual(CheckoutSolution().checkout("aaabb"), 175, "Not correct!")
+        self.assertEqual(CheckoutSolution().checkout("aaabb"), 175, "Not correct!")
+        
         #self.assertEqual(Calculator.add(-6, 4), -2, "Not correct!")
         #self.assertEqual(Calculator.add(6, -4), 2, "Not correct!")
         #self.assertEqual(Calculator.add(-6, -4), -10, "Not correct!")
